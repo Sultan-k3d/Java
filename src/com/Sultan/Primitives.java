2aa922dacc236e2dc633e3a59c003ef238100eee
Sultan-k3d <sultanali.xdr@gmail.com>
2025-12-12 08:57:08 +0530

    Started with Java

---
+++ src/com/Sultan/Primitives.java
@@ -0,0 +1,12 @@
+package com.Sultan;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNp = 223;
+        char star = '*';
+        float marks = 89.9f;
+        double doubleVariable = 23.4;
+        long longValue = 4567890L;
+        boolean check = true;
+    }
+}
